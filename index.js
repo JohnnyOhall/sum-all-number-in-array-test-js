8e16953420b3ff03f5e94bcd44d3181b39506380
John O'Halloran <john.ohalloran@telus.com>
2022-12-22 13:48:04 -0500

    add sumItems function body

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = array => {
+  const flatArray = flatten(array), end = flatArray.length - 1;
+  let total = 0, start = 0;
+
+  const count = (array, start, end) => {
+    total += array[start];
+    return start !== end &&
+      (start++, count(array, start, end));
+  };
+
+  count(flatArray, start, end);
+
+  return total;
+};
+
+
+const flatten = array => {
+  const output = [];
+
+  const checkForArray = input => {
+    return input.forEach(x =>
+      Array.isArray(x) ?
+        checkForArray(x) :
+        output.push(x));
+  };
+
+  checkForArray(array);
+
+  return output;
+};
+
 
 module.exports = sumItems;
